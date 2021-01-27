81ab33d63fa667365df6c4937067ba5ce657fbd0
festinalentee <karolina.bienkowska550@gmail.com>
2021-01-27 14:16:55 +0100

    CSS styles

---
+++ public/views/login.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=1280, maximum-scale=1.0" />
+    <link rel="shortcut icon" href="./img/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="public/css/style.css" />
+    <title>Tastia</title>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="web1-login screen">
+        <img class="logo" src="img/logooo@1x.png" />
+        <div class="component-login">
+          <img class="path-1" src="img/path-1@1x.png" />
+          <img class="line-1" src="img/line-1@1x.png" />
+          <img class="line-2" src="img/line-1@1x.png" />
+          <a href="web3-home.html"> <div class="login-button"></div></a>
+          <input class="email-input" name="email" placeholder="email@email.com" type="text" required />
+          <input class="password-input" name="password" placeholder="password" type="password" required />
+          <div class="inscription-login">login</div>
+          <a href="web2-register.html"> <div class="register-button"></div></a>
+          <div class="inscription-register">register</div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
