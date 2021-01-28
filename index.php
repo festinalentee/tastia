ddbc1f0509855fd9adecc7c27fe4e5a78f25c4da
festinalentee <karolina.bienkowska550@gmail.com>
2021-01-28 20:25:38 +0100

    refactor index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url( $path, PHP_URL_PATH);
 
 Routing::get('index', 'DefaultController');
+Routing::get('login', 'DefaultController');
 Routing::get('home', 'DefaultController');
 
 Routing::run($path);
