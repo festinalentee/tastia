50931ef01b383a9de2ea1e30aef8f84e5ab31dad
festinalentee <karolina.bienkowska550@gmail.com>
2021-01-29 18:04:21 +0100

    authorization

--- Routing.php
+++ Routing.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'src/controllers/DefaultController.php';
+require_once 'src/controllers/SecurityController.php';
 
 class Routing
 {
@@ -9,6 +10,10 @@ class Routing
 
     public static function get($url, $view)
     {
+        self::$routes[$url] = $view;
+    }
+
+    public static function post($url, $view) {
         self::$routes[$url] = $view;
     }
 
