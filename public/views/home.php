04c11b646f92d99473d6d858caad701b63b0920d
festinalentee <karolina.bienkowska550@gmail.com>
2021-01-31 15:31:50 +0100

    refactor home - upload

--- public/views/home.php
+++ public/views/home.php
@@ -9,89 +9,59 @@
 </head>
 
 <body>
-<div class="base-container">
-    <nav>
-        <img src="public/img/logooo@1x.png">
-        <ul>
-            <li>
-                <i class="fas fa-home"></i>
-                <a href="#" class="selected">All recipes</a>
-            </li>
-            <li>
-                <i class="fas fa-coffee"></i>
-                <a href="#" class="button">Breakfast</a>
-            </li>
-            <li>
-                <i class="fas fa-apple-alt"></i>
-                <a href="#" class="button">Lunch</a>
-            </li>
-            <li>
-                <i class="fas fa-fish"></i>
-                <a href="#" class="button">Dinner</a>
-            </li>
-            <li>
-                <i class="fas fa-ice-cream"></i>
-                <a href="#" class="button">Dessert</a>
-            </li>
-            <li>
-                <i class="fas fa-glass-martini-alt"></i>
-                <a href="#" class="button">Drink</a>
-            </li>
-            <li>
-                <i class="fas fa-sign-out-alt"></i>
-                <a href="#" class="button">Logout</a>
-            </li>
-        </ul>
-    </nav>
-    <main>
-        <header>
-            <div class="search-bar">
-                <form>
-                    <input placeholder="search recipe">
-                </form>
-            </div>
-            <div class="add-recipe">
-                <i class="fas fa-plus"></i> Add a recipe
-            </div>
-        </header>
-        <section class="allRecipes">
-            <div id="recipe-1">
-                <img src="public/img/image@1x.png">
-                <div>
-                    <h2>Sandwiches</h2>
+    <div class="base-container">
+        <nav>
+            <img src="public/img/logooo@1x.png">
+            <ul>
+                <li>
+                    <i class="fas fa-home"></i>
+                    <a href="#" class="selected">All recipes</a>
+                </li>
+                <li>
+                    <i class="fas fa-coffee"></i>
+                    <a href="#" class="button">Breakfast</a>
+                </li>
+                <li>
+                    <i class="fas fa-apple-alt"></i>
+                    <a href="#" class="button">Lunch</a>
+                </li>
+                <li>
+                    <i class="fas fa-fish"></i>
+                    <a href="#" class="button">Dinner</a>
+                </li>
+                <li>
+                    <i class="fas fa-ice-cream"></i>
+                    <a href="#" class="button">Dessert</a>
+                </li>
+                <li>
+                    <i class="fas fa-glass-martini-alt"></i>
+                    <a href="#" class="button">Drink</a>
+                </li>
+                <li>
+                    <i class="fas fa-sign-out-alt"></i>
+                    <a href="#" class="button">Logout</a>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <header>
+                <div class="search-bar">
+                    <form>
+                        <input placeholder="search recipe">
+                    </form>
                 </div>
-            </div>
-            <div id="recipe-2">
-                <img src="public/img/image-1@1x.png">
-                <div>
-                    <h2>Pasta</h2>
+                <div class="add-recipe">
+                    <a href="#" class="add-recipe-button"><i class="fas fa-plus"></i> Add recipe</a>
                 </div>
-            </div>
-            <div id="recipe-3">
-                <img src="public/img/image-2@1x.png">
-                <div>
-                    <h2>Sushi</h2>
-                </div>
-            </div>
-            <div id="recipe-4">
-                <img src="public/img/image-3@1x.png">
-                <div>
-                    <h2>Salad</h2>
-                </div>
-            </div>
-            <div id="recipe-5">
-                <img src="public/img/image-4@1x.png">
-                <div>
-                    <h2>Smoothie</h2>
-                </div>
-            </div>
-            <div id="recipe-6">
-                <img src="public/img/image-5@1x.png">
-                <div>
-                    <h2>Apple pie</h2>
-                </div>
-            </div>
-        </section>
-    </main>
-</div>
+            </header>
+            <section class="allRecipes">
+                <div id="recipe-1">
+                    <img src="public/uploads/<?= $recipe->getImage() ?>">
+                    <div>
+                        <h2><?= $recipe->getTitle() ?></h2>
+                    </div>
+                </div> 
+            </section>
+        </main>
+    </div>
 </body>
