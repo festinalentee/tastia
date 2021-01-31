ca4999b73f1ee7ed12b91fcaa5e2f3babf73c239
festinalentee <karolina.bienkowska550@gmail.com>
2021-01-31 22:19:25 +0100

    index

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url( $path, PHP_URL_PATH);
 
 Routing::get('', 'DefaultController');
-Routing::post('login', 'SecurityController');
 Routing::get('home', 'DefaultController');
+Routing::post('login', 'SecurityController');
 Routing::post('addRecipe', 'RecipeController');
 
 Routing::run($path);
