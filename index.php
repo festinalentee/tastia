49990e252474a65b68bd4f1ee9309f561253655e
festinalentee <karolina.bienkowska550@gmail.com>
2021-02-02 16:16:25 +0100

    added registration and validation

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url( $path, PHP_URL_PATH);
 
 Routing::get('', 'DefaultController');
-Routing::get('home', 'DefaultController');
+Routing::get('recipes', 'RecipeController');
 Routing::post('login', 'SecurityController');
 Routing::post('addRecipe', 'RecipeController');
+Routing::post('register', 'SecurityController');
 
 Routing::run($path);
---
+++ public/views/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=1280, maximum-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/login.css">
+    <script type="text/javascript" src="/public/js/script.js" defer></script>
+    <title>Tastia</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="public/img/logooo@1x.png">
+        </div>
+        <div class="login-container">
+            <form class="register" action="register" method="POST">
+                <div class="messages">
+                    <?php
+                    if(isset($messages)){
+                        foreach($messages as $message) {
+                            echo $message;
+                        }
+                    }
+                    ?>
+                </div>
+                <input name="email" type="text" placeholder="email@email.com">
+                <input name="password" type="password" placeholder="password">
+                <input name="confirmedPassword" type="password" placeholder="confirm password">
+                <input name="name" type="text" placeholder="name">
+                <input name="surname" type="text" placeholder="surname">
+                <button type="submit">REGISTER</button>
+            </form>
+        </div>
+    </div>
+</body>
